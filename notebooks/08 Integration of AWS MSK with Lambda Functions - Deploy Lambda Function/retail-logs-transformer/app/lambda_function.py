d2b15d093e6a743b0e68723717fa7d1906a2e904
EC2 Default User <ec2-user@ip-172-31-70-113.ec2.internal>
2022-08-16 09:14:55 +0000

    Updated Content

--- notebooks/08 Integration of AWS MSK with Lambda Functions - Deploy Lambda Function/retail-logs-transformer/app/lambda_function.py
+++ notebooks/08 Integration of AWS MSK with Lambda Functions - Deploy Lambda Function/retail-logs-transformer/app/lambda_function.py
@@ -1,8 +1,10 @@
 import urllib
 import pandas as pd
+import os
 
 def lambda_handler(event, context):
     print('Processing Started')
+    target_bucket_name = os.environ.get('TGT_BUCKET_NAME')
     for rec in event['Records']:
         bucket_name = rec['s3']['bucket']['name']
         object_key = urllib.parse.unquote(rec['s3']['object']['key'])
@@ -23,8 +25,8 @@ def lambda_handler(event, context):
             month = list(key)[1]
             dayofmonth = list(key)[2]
             file_name = f'{orig_file_name}.snappy.parquet'
-            df_by_key.to_parquet(f's3://{bucket_name}/silver/retail_logs/year={year}/month={month}/dayofmonth={dayofmonth}/{file_name}')
-        print(f'{df.shape[0]} records in s3://{bucket_name}/{object_key} are converted to Parquet Format')
+            df_by_key.to_parquet(f's3://{target_bucket_name}/silver/retail_logs/year={year}/month={month}/dayofmonth={dayofmonth}/{file_name}')
+        print(f'{df.shape[0]} records in s3://{target_bucket_name}/{object_key} are converted to Parquet Format')
     print('Processing is successfully done')
     return {
         'statusCode': 200,
